d2bca0a08b1d33c7b5f4dff67ed736448e4d19ec
antskat <donaldsmak7@gmail.com>
2023-12-05 16:59:00 +0200

    fix something

--- js/main.js
+++ js/main.js
@@ -1,86 +1,10 @@
-// scroll to
-var main = document.getElementById("main");
-var advantages = document.getElementById("advantages");
-var review = document.getElementById("review");
-var contacts = document.getElementById("contacts");
-var order = document.getElementById("order");
-
-var mainPage = document.getElementById("main-page");
-var advantagesPage = document.getElementById("advantages-page");
-var reviewPage = document.getElementById("reviews-page");
-var contactsPage = document.getElementById("contacts-page");
-var orderPage = document.getElementById("order-page");
+let mainPage = document.getElementById("main-page");
+let benefitsPage = document.getElementById("benefits-page");
+let advantagesPage = document.getElementById("advantages-page");
+let reviewPage = document.getElementById("reviews-page");
+let contactsPage = document.getElementById("contacts-page");
+let orderPage = document.getElementById("order-page");
 const links = document.querySelectorAll(".nav-link");
-function mainScroll() {
-  if (main) {
-    main.scrollIntoView({ behavior: "smooth" }, 1500);
-    mainPage.classList.add("text-active");
-    advantagesPage.classList.remove("text-active");
-    reviewPage.classList.remove("text-active");
-    contactsPage.classList.remove("text-active");
-    orderPage.classList.remove("text-active");
-  } else {
-    main.scrollIntoView({ behavior: "smooth" }, 1500);
-    console.log("no main");
-  }
-  console.log("main");
-}
-function advantagesScroll() {
-  if (advantages) {
-    advantages.scrollIntoView({ behavior: "smooth" }, 1500);
-    mainPage.classList.remove("text-active");
-    advantagesPage.classList.add("text-active");
-    reviewPage.classList.remove("text-active");
-    contactsPage.classList.remove("text-active");
-    orderPage.classList.remove("text-active");
-  } else {
-    advantages.scrollIntoView({ behavior: "smooth" }, 1500);
-    console.log("no advantages");
-  }
-  console.log("advantages");
-}
-function reviewScroll() {
-  if (review) {
-    review.scrollIntoView({ behavior: "smooth" }, 1500);
-    mainPage.classList.remove("text-active");
-    advantagesPage.classList.remove("text-active");
-    reviewPage.classList.add("text-active");
-    contactsPage.classList.remove("text-active");
-    orderPage.classList.remove("text-active");
-  } else {
-    review.scrollIntoView({ behavior: "smooth" }, 1500);
-    console.log("no review");
-  }
-  console.log("review");
-}
-function contactsScroll() {
-  if (contacts) {
-    contacts.scrollIntoView({ behavior: "smooth" }, 1500);
-    mainPage.classList.remove("text-active");
-    advantagesPage.classList.remove("text-active");
-    reviewPage.classList.remove("text-active");
-    contactsPage.classList.add("text-active");
-    orderPage.classList.remove("text-active");
-  } else {
-    contacts.scrollIntoView({ behavior: "smooth" }, 1500);
-    console.log("no contacts");
-  }
-  console.log("contacts");
-}
-function orderScroll() {
-  if (order) {
-    order.scrollIntoView({ behavior: "smooth" }, 1500);
-    mainPage.classList.remove("text-active");
-    advantagesPage.classList.remove("text-active");
-    reviewPage.classList.remove("text-active");
-    contactsPage.classList.remove("text-active");
-    orderPage.classList.add("text-active");
-  } else {
-    order.scrollIntoView({ behavior: "smooth" }, 1500);
-    console.log("no order");
-  }
-  console.log("order");
-}
 document.addEventListener("scroll", function () {
   var windowHeight = window.innerHeight;
   var sections = document.querySelectorAll("section, footer");
@@ -90,30 +14,35 @@ document.addEventListener("scroll", function () {
     if (rect.top < windowHeight && rect.bottom >= 0) {
       if (section.id === "main") {
         mainPage.classList.add("text-active");
+        benefitsPage.classList.remove("text-active");
         advantagesPage.classList.remove("text-active");
         reviewPage.classList.remove("text-active");
         contactsPage.classList.remove("text-active");
         orderPage.classList.remove("text-active");
       } else if (section.id === "advantages") {
         mainPage.classList.remove("text-active");
+        benefitsPage.classList.add("text-active");
         advantagesPage.classList.add("text-active");
         reviewPage.classList.remove("text-active");
         contactsPage.classList.remove("text-active");
         orderPage.classList.remove("text-active");
       } else if (section.id === "review") {
         mainPage.classList.remove("text-active");
+        benefitsPage.classList.remove("text-active");
         advantagesPage.classList.remove("text-active");
         reviewPage.classList.add("text-active");
         contactsPage.classList.remove("text-active");
         orderPage.classList.remove("text-active");
       } else if (section.id === "contacts") {
         mainPage.classList.remove("text-active");
+        benefitsPage.classList.remove("text-active");
         advantagesPage.classList.remove("text-active");
         reviewPage.classList.remove("text-active");
         contactsPage.classList.add("text-active");
         orderPage.classList.remove("text-active");
       } else if (section.id === "order") {
         mainPage.classList.remove("text-active");
+        benefitsPage.classList.remove("text-active");
         advantagesPage.classList.remove("text-active");
         reviewPage.classList.remove("text-active");
         contactsPage.classList.remove("text-active");
@@ -122,4 +51,3 @@ document.addEventListener("scroll", function () {
     }
   });
 });
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
